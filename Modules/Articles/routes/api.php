6c15fcbd648e4ce8830b99165b7066ae4208380f
Emenegildo Marques <emenegildomarques0@gmail.com>
2025-11-12 02:24:05 +0100

    fix(routes): resolve route naming conflict for api.articles.index

--- Modules/Articles/routes/api.php
+++ Modules/Articles/routes/api.php
@@ -1,34 +1,31 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 use Modules\Articles\app\Http\Controllers\Admin\ArticleController as AdminArticleController;
 use Modules\Articles\app\Http\Controllers\ArticlesController;
 use Modules\Articles\app\Http\Controllers\CategoryController;
 
 Route::prefix('v1')->group(function () {
-
     // Rotas públicas de artigos
     Route::prefix('articles')->group(function () {
-        Route::get('/', [ArticlesController::class, 'index']);
-        Route::get('/{article:slug}', [ArticlesController::class, 'show']);
+        Route::get('/', [ArticlesController::class, 'index'])->name('articles.index');
+        Route::get('/{article:slug}', [ArticlesController::class, 'show'])->name('articles.show');
     });
 
     // Rotas administrativas protegidas por autenticação
     Route::middleware('auth:sanctum')->group(function () {
-
         // Artigos (privados)
-        Route::prefix('admin/articles')->group(function () {
-            Route::apiResource('/', AdminArticleController::class)->parameters([
-                '' => 'article'
-            ]);
-            Route::post('/{id}/restore', [AdminArticleController::class, 'restore']);
+        Route::prefix('admin')->group(function () {
+            Route::apiResource('articles', AdminArticleController::class)->parameters([
+                'articles' => 'article'
+            ])->names('admin.articles');
+            Route::post('articles/{id}/restore', [AdminArticleController::class, 'restore'])->name('admin.articles.restore');
         });
 
         // Categorias (privadas)
-        Route::prefix('admin/categories')->group(function () {
-            Route::apiResource('/', CategoryController::class)->parameters([
-                '' => 'category'
-            ]);
+        Route::prefix('admin')->group(function () {
+            Route::apiResource('categories', CategoryController::class)->parameters([
+                'categories' => 'category'
+            ])->names('admin.categories');
         });
     });
 });
--- Modules/Articles/routes/web.php
+++ Modules/Articles/routes/web.php
@@ -3,6 +3,3 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Articles\Http\Controllers\ArticlesController;
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('articles', ArticlesController::class)->names('articles');
-});
